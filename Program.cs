f8b241d3cfa45b5b1ce144008048c7f325f05459
Oksana <jelamskaya891@gmail.com>
2023-03-22 10:07:37 +0200

    Zadaca 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает,
+// какое исло большее и какое меньшее.
+
+Console.Clear();
+Console.Write("Введите первое число:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n > n2)
+   Console.WriteLine($"max{n}");
+else
+  Console.WriteLine($"max{n2}");   
